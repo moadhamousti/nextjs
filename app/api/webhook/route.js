699a6298d0a8db8c1ec82e0ca15f74d9d86454f8
moadhamousti <118165767+moadhamousti@users.noreply.github.com>
2024-01-07 10:12:41 +0100

    Update

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -2,13 +2,20 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { createOrUpdateUser, deleteUser } from "@lib/actions/user";
 
+
+export async function GET() {
+  return Response.json({ message: "Hello World!" });
+}
+
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-  if (!WEBHOOK_SECRET) {
+
+  if (WEBHOOK_SECRET) {
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      { message: "Received" }
+      // "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
   }
 
