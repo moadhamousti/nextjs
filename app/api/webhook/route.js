4f5bc0a8932a0df659190c89fd848b8e15ed0a3f
moadhamousti <118165767+moadhamousti@users.noreply.github.com>
2024-01-07 10:16:33 +0100

    Update

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -3,15 +3,14 @@ import { headers } from "next/headers";
 import { createOrUpdateUser, deleteUser } from "@lib/actions/user";
 
 
+
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-
-  if (WEBHOOK_SECRET) {
+  if (!WEBHOOK_SECRET) {
     throw new Error(
-      { message: "Received" }
-      // "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
   }
 
