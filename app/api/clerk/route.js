65cb6b1044013cdfbac66a5f0bca818e3ef04cbe
moadhamousti <118165767+moadhamousti@users.noreply.github.com>
2024-01-09 09:36:36 +0100

    Test

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -1,11 +1,5 @@
-// app/api/clerk/route.js
-import { WebhookEvent } from "@clerk/nextjs/server";
-
 export async function POST(request) {
   const payload = await request.json();
   console.log(payload);
-}
-
-export async function GET() {
-  return Response.json({ message: "Hello World!" });
+  return Response.json({ message: "Received" });
 }
